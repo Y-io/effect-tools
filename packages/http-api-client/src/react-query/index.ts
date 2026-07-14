cff43b5d0f8253dbaef16db4283dd56a8d511dbb
henry <yiobincn@gmail.com>
2026-07-14 16:22:31 +0400

    refactor: narrow react query exports

--- packages/http-api-client/src/react-query/index.ts
+++ packages/http-api-client/src/react-query/index.ts
@@ -1,20 +1,4 @@
 export { EffectDefect, type EffectRuntimeLoader } from "./effect"
-export {
-  makeEffectMutationOptions,
-  type EffectMutationOptions,
-  type MutationDescriptor,
-  type MutationDescriptorOptions,
-  type MutationEffect,
-} from "./mutation"
-export {
-  makeEffectQueryOptions,
-  type DescriptorOptions,
-  type EffectQueryKey,
-  type EffectQueryOptions,
-  type EmptyQueryDescriptor,
-  type Json,
-  type JsonObject,
-  type QueryDescriptor,
-  type QueryEffect,
-} from "./query"
+export { makeEffectMutationOptions, type EffectMutationOptions } from "./mutation"
+export { makeEffectQueryOptions, type EffectQueryOptions } from "./query"
 export { makeEffectQueryRuntime } from "./react"
